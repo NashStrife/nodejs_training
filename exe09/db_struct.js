65915553ea466ec65ce091a5a72386db667e95af
NashStrife <jac.sebastien@gmail.com>
2016-06-02 15:19:13 +0200

    mongoDB initiation

---
+++ exe09/db_struct.js
@@ -0,0 +1,120 @@
+db.companies.insert({
+    "name" : "facebook",
+    "address" : {
+	        "street" : "Rich Street",
+	        "nb" : 100,
+	        "cp" : 2048,
+	        "town" : "Money",
+	        "country" : "United states"
+    },
+    "financial_data" : {
+        "turnover" : 100000000,
+        "employees" : 250
+    },
+    "creation_year" : 2004
+})
+db.companies.insert({
+    "name" : "google",
+    "address" : {
+	        "street" : "Sparrow Street",
+	        "nb" : 254,
+	        "cp" : 6528,
+	        "town" : "Ney York",
+	        "country" : "United states"
+    },
+    "financial_data" : {
+        "turnover" : 66000000000,
+        "employees" : 57000
+    },
+    "creation_year" : 1998
+})
+db.companies.insert({
+    "name" : "blizzard",
+    "address" : {
+	        "street" : "Machin Street",
+	        "nb" : 28,
+	        "cp" : 2368,
+	        "town" : "Irvine",
+	        "country" : "Californie"
+    },
+    "financial_data" : {
+        "turnover" : 10000000,
+        "employees" : 4700
+    },
+    "creation_year" : 1991
+})
+db.companies.insert({
+    "name" : "ea games",
+    "address" : {
+	        "street" : "Truc Street",
+	        "nb" : 21258,
+	        "cp" : 2458,
+	        "town" : "Redwood",
+	        "country" : "Etats unis"
+    },
+    "financial_data" : {
+        "turnover" : 3000000000,
+        "employees" : 8300
+    },
+    "creation_year" : 1982
+})
+db.companies.insert({
+    "name" : "twitter",
+    "address" : {
+	        "street" : "rue du blanc",
+	        "nb" : 24,
+	        "cp" : 256,
+	        "town" : "San Francisco",
+	        "country" : "Etats unis"
+    },
+    "financial_data" : {
+        "turnover" : 5000000000,
+        "employees" : 3900
+    },
+    "creation_year" : 2006
+})
+db.companies.insert({
+    "name" : "amazon",
+    "address" : {
+	        "street" : "rue de l'orange",
+	        "nb" : 235,
+	        "cp" : 2569,
+	        "town" : "Seattle",
+	        "country" : "Etats unis"
+    },
+    "financial_data" : {
+        "turnover" : 74000000000,
+        "employees" : 230000
+    },
+    "creation_year" : 1994
+})
+db.companies.insert({
+    "name" : "ldlc",
+    "address" : {
+	        "street" : "rue de la carte",
+	        "nb" : 25,
+	        "cp" : 63584,
+	        "town" : "Dardilly",
+	        "country" : "France"
+    },
+    "financial_data" : {
+        "turnover" : 285000000,
+        "employees" : 500
+    },
+    "creation_year" : 1996
+})
+db.companies.insert({
+    "name" : "Microsoft",
+    "address" : {
+	        "street" : "rue de lu dos",
+	        "nb" : 65,
+	        "cp" : 4589,
+	        "town" : "Redmond",
+	        "country" : "Etats unis"
+    },
+    "financial_data" : {
+        "turnover" : 93000000000,
+        "employees" : 117354
+    },
+    "creation_year" : 1975
+})

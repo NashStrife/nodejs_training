eb5fcb429c8b55c6bd360bc538b1d82c8bad703b
NashStrife <jac.sebastien@gmail.com>
2016-06-15 15:13:06 +0200

    cleaning the exe02

--- Angular/exe02/js/main.js
+++ Angular/exe02/js/main.js
@@ -31,27 +31,3 @@ oneApp.controller('childController', ['$scope', function($scope){
 oneApp.controller('grandController', ['$scope', function($scope){
     $scope.name = "Jonathan";
 }]);
-
-oneApp.controller('shopController', ['$scope', function($scope){
-    $scope.clothing=[
-        {
-            'name' : 'Team Liquid Spring 2016 Jersey', 
-            'size' : 'M',
-            'price' : 64.17
-        },
-        {
-            'name' : 'J!NX 8-Bit Skull Boss Premium Tee', 
-            'size' : 'S',
-            'price' : 20.16
-        },
-        {
-            'name' : 'Minecraft Creeper Premium Zip-up Hoodie', 
-            'size' : 'L',
-            'price' : 55
-        },
-        {
-            'name' : 'Team Liquid Softshell Jacket', 
-            'size' : 'M',
-            'price' : 119.17
-        }];
-}]);

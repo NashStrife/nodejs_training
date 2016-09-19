a4f744777ed0048bc247ae8ad88ca80d80038fe3
NashStrife <jac.sebastien@gmail.com>
2016-09-19 12:11:48 +0200

    node hapi works with angular and mongoose

--- recap/restos-hapi/server/api/restos/controller.js
+++ recap/restos-hapi/server/api/restos/controller.js
@@ -6,7 +6,6 @@ let model = require('./model');
 
 exports.get = function(req, res, next) {
     logger.log("Controller GET");
-    // res({message : "Controller GET"});
     // find() function of the model thanks to Mongoose [cf : http://mongoosejs.com/docs/queries.html]
     model.find()
     // once the query is done, do the following action thanks to a "promise"
@@ -19,79 +18,71 @@ exports.get = function(req, res, next) {
 
 exports.post = function(req, res, next) {
     logger.log("Controller POST");
-    // res({message : "Controller POST"});
     
     // we create a new model with the data to add to the db
-    let resto = new model(req.body);
+    let resto = new model(req.payload);
     
     // and add it to the db
     resto.save(function(err, data) {
-        let message = {
-            message: 'Document saved',
-            success : true
-        };
         if (err) {
-            message.message = err;
-            message.success = false;
+            res({error_code:1,err_desc:err});
+            logger.warn(err);    
+            return;
         }
-        res(message);
+        res({error_code:0,message: 'Document saved'});
+        logger.log('Document saved');
     });
 };
 
 
 exports.update = function(req, res, next) {
     logger.log("Controller UPDATE");
-    res({message : "Controller UPDATE"});
-    
-    // // update the data corresponding to the id with the new one in the request
-    // model.findByIdAndUpdate(req.body._id, req.body, 
-    //     function(err, doc) {
-    //         let message = {
-    //             message: 'Document upated',
-    //             success : true
-    //         };
-    //         if (err) {
-    //             message.message = err;
-    //             message.success = false;
-    //         }
-    //         res.json(message);
-    //     }
-    // );
+    // res({message : "Controller UPDATE"});
+    logger.log(req.payload);
+    // update the data corresponding to the id with the new one in the request
+    model.findByIdAndUpdate(req.payload._id, req.payload, 
+        function(err, doc) {
+            if (err) {
+                res({error_code:1,err_desc:err});
+                logger.warn(err);    
+                return;
+            }
+            res({error_code:0,message: 'Document updated'});
+            logger.log('Document updated');
+            logger.log(doc);
+        }
+    );
 };
 
 exports.deleteById = function(req, res, next) {
     logger.log("Controller DELETE");
-    res({message : "Controller DELETE"});
     
-    // // get id from the parameters sent in the url
-    // model.findByIdAndRemove(req.params.id,
-    //     function(err, doc) {
-    //         let message = {
-    //             message: 'Document removed',
-    //             success : true
-    //         };
-    //         if (err) {
-    //             message.message = err;
-    //             message.success = false;
-    //         }
-    //         res.json(message);
-    //     }
-    // );
+    // get id from the parameters sent in the url
+    model.findByIdAndRemove(req.params.id,
+        function(err, doc) {
+            if (err) {
+                res({error_code:1,err_desc:err});
+                logger.warn(err);    
+                return;
+            }
+            res({error_code:0,message: 'Document deleted'});
+            logger.log('Document deleted');
+        }
+    );
 }
 
 exports.dynamicSearch = function(req, res, next) {
     logger.log("Controller dynamicSearch");
-    res({message : "Controller dynamicSearch"});
 
-    // let query = req.query;
-    // console.log(req.query);
-    // model.find(query)
-    // .then(function(docs){
-    //     // the result is not empty we have a corresponding result
-    //     if(docs.length) {
-    //         res.json(docs);
-    //     } else {
-    //         res.json({message:`No result for the query ${JSON.stringify(query)}`});
-    //     }
-    // });
+    let query = req.query;
+    console.log(req.query);
+    model.find(query)
+    .then(function(docs){
+        // the result is not empty we have a corresponding result
+        if(docs.length) {
+            res({error_code:0,data: docs});
+        } else {
+            res({error_code:1,err_desc:`No result for the query ${JSON.stringify(query)}`});
+        }
+    });
 };
--- recap/restos-hapi/server/api/restos/routes.js
+++ recap/restos-hapi/server/api/restos/routes.js
@@ -3,29 +3,9 @@
 let controller = require('./controller');
 
 module.exports = [
-    { method: 'GET', path: '/api/restos/', handler: controller.get },
-    { method: 'POST', path: '/api/restos/', handler: controller.post },
-    { method: 'PUT', path: '/api/restos/', handler: controller.update },
+    { method: 'GET', path: '/api/restos', handler: controller.get },
+    { method: 'POST', path: '/api/restos', handler: controller.post },
+    { method: 'PUT', path: '/api/restos', handler: controller.update },
     { method: 'GET', path: '/api/restos/search', handler: controller.dynamicSearch },
     { method: 'DELETE', path: '/api/restos/{id}', handler: controller.deleteById }
 ];
-
-// let router = require(`${process.cwd()}/server/server`);
-// let controller = require('./controller');
-
-// router.route({
-//     method: 'GET',
-//     path: '/restos/{name}',
-//     handler: function (request, reply) {
-//         // URI encode the name parameter to prevent content injection attacks
-//         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-//     }
-// });
-
-// // router.route({
-// //     method: 'GET',
-// //     path: '/',
-// //     handler: controller.get
-// // });
-
-// module.exports = router;
